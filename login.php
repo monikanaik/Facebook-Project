3886925d9a88d2eee850d6394241232a5dae7206
monikanaik <monikanaik16@gmail.com>
2016-07-04 12:24:42 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,33 @@
+<html>
+
+	<head>
+		<title>Facebook</title>
+	</head>
+	<body>
+		
+		<center><font size="6" face="Verdana"><b>FACEBOOK WORLD</b></font></center>
+		
+		       <center>
+			<img src="login.png">
+		
+		
+		
+		      <form action="checkuser.php" method ="post">
+		      <table>
+                          <center>
+			
+			<tr><td><center><font size="3" face="Verdana">Email Address or Phone Number<b></b></font></center></td>
+		</tr><tr><td><input type="text" name="user"></td></tr>
+			<tr><td>Password</td></tr><tr><td><input type="password" name="pwd"></td></tr>
+			      </center>
+                       
+			 <tr align="center"><td colspan="2"><input type="submit" value="SUBMIT"></td></tr>
+		</table>
+		</form>
+		</center>
+		
+		<center> <a color="red" href='registform.php'>Click here for creating account</a></center>
+		
+	</body>
+
+</html>
