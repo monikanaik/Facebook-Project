dc0b4454a52ab34d63f822a7545113639d4160b0
monikanaik <monikanaik16@gmail.com>
2016-07-04 12:27:40 +0530

    Create navigation.php

---
+++ navigation.php
@@ -0,0 +1,7 @@
+<div id="navigation">
+	<div class="navitem" style="border-left:none"><a href="index.php?action=home">Home</a></div>
+    <div class="navitem"><a href="mm.php">Find Friend</a></div>
+	 <div class="navitem"><a href="nn.php">Profile</a></div>
+    
+    <div class="navitem"><a href="logout.php">Logout</a></div>
+</div>
